adef455775a489f76bd5e009e3e0cba77ea01e08
cmg1eroles <cmg1eroles@gmail.com>
2018-02-11 00:43:38 +0800

    Initial project skeleton

---
+++ src/main/java/ph/edu/dlsu/securde/SECURDE_MP/SecurdeMpApplication.java
@@ -0,0 +1,12 @@
+package ph.edu.dlsu.securde.SECURDE_MP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurdeMpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurdeMpApplication.class, args);
+	}
+}
