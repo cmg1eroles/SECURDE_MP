de5bcc1c379398acfa8faf2956e063d65b6a32f0
Cheska <cheska_alejandrino@dlsu.edu.ph>
2018-03-07 18:56:04 +0800

    changed design and deleted old design

--- src/main/resources/static/js/adopt.js
+++ src/main/resources/static/js/adopt.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
     $('#adopt-btn').click(function(){
+        console.log("ADOPT CLICKED!")
+        alert("You Have Adopted a pet!");
         var formans = {
             'adopter_id': $('#h-uname').attr('data-uid'),
             'pet_id': $('#adopt-btn').attr('data-pid')
