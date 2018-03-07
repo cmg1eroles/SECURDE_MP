0cd0b9a361526ded8bed57915d7f02948a62379f
cmg1eroles <cmg1eroles@gmail.com>
2018-03-07 18:00:12 +0800

    fresh start

--- src/main/java/ph/edu/dlsu/securde/SECURDE_MP/repository/PostForumRepository.java
+++
@@ -1,9 +0,0 @@
-package ph.edu.dlsu.securde.SECURDE_MP.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import ph.edu.dlsu.securde.SECURDE_MP.model.PostForum;
-
-@Repository
-public interface PostForumRepository extends JpaRepository<PostForum, Long> {
-}
--- src/main/webapp/WEB-INF/jsp/errorpage.jsp
+++
@@ -1,8 +0,0 @@
-<html>
-<head>
-    <title>Error</title>
-</head>
-<body class = "site">
-    <div>Boops! You seem to have stumbled upon a non-existent page.</div>
-</body>
-</html>
